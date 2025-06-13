175880b5c7a01d03da2e0393fb96e23d80ac8fcf
emmaimade <emmaimade14@gmail.com>
2025-06-13 18:32:04 +0100

    Implement user register and login routes

--- controllers/v1/userController.js
+++ controllers/v1/userController.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import db from "../../config/db";
+import db from "../../config/db.js";
 
 export const register = async (req, res) => {
   try {
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 import db from './config/db.js';
+import userRoutes from './routes/v1/userRoutes.js';
 
 const app = express();
 const port = process.env.PORT;
@@ -9,9 +10,16 @@ const port = process.env.PORT;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
+// ========================================
+// API ROUTES - GENERAL ENDPOINTS
+// ========================================
+
+// health check
 app.get('/health', (req, res) => {
     res.send('ok');
 })
+
+app.use('/v1/user', userRoutes);
 
 app.listen(port, () => {
     console.log(`server is running on http://localhost:${port}`);
