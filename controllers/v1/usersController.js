9b7a083e0d2ea2c05a095cfa673b1b1133ca4a9a
emmaimade <emmaimade14@gmail.com>
2025-06-13 17:14:25 +0100

    Implement user register and login logic

---
+++ controllers/v1/usersController.js
@@ -0,0 +1,61 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import db from "../../config/db";
+
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const existingUser = await db.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email]
+    );
+
+    if (existingUser.rows.length > 0) {
+      return res
+        .status(400)
+        .json({ message: "User already exists, try logging in" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await db.query(
+      "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *",
+      [name, email, hashedPassword]
+    );
+
+    res
+      .status(201)
+      .json({ message: "User registered successfully", user: newUser.rows[0] });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const result = await db.query("SELECT * FROM users WHERE email = $1", [email]);
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: "User not found, try registering" });
+        } else {
+            const user = result.rows[0];
+            const passwordMatch = await bcrypt.compare(password, user.password);
+
+            if (passwordMatch) {
+                const token = jwt.sign({ id: user.id, role: user.role}, process.env.JWT_SECRET, { expiresIn: '1d' });
+                res.status(200).json({ message: "Login successful", token });
+            } else {
+                res.status(401).json({ message: "Invalid credentials" });
+            }
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+import db from './config/db.js';
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get('/health', (req, res) => {
+    res.send('ok');
+})
 
 app.listen(port, () => {
     console.log(`server is running on http://localhost:${port}`);
