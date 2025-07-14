29b1fd05687a0a6cf26bd9937e074066303079c3
emmaimade <emmaimade14@gmail.com>
2025-07-14 23:31:07 +0100

    Implement getOrderHistory route

--- routes/v1/orderRoutes.js
+++ routes/v1/orderRoutes.js
@@ -4,6 +4,7 @@ import {
   verifyPayment,
   getOrders,
   getOrder,
+  getOrderHistory,
 } from "../../controllers/v1/orderController.js";
 import { auth } from "../../middleware/auth.js";
 
@@ -13,5 +14,6 @@ router.post("/", auth, createOrder);
 router.get("/payment/verify/:reference", auth, verifyPayment);
 router.get("/", auth, getOrders);
 router.get("/:id", auth, getOrder);
+router.get("/:orderId/history", auth, getOrderHistory);
 
 export default router;
